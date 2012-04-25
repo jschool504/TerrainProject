c307a632c0d70dfb86063a5203895e4b601210e0
Jeremy School <jschool504@gmail.com>
2012-04-25 13:26:28 -0400

    Initial Commit

---
+++ TerrainProject/TPAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  TPAppDelegate.m
+//  TerrainProject
+//
+//  Created by Jeremy School on 4/25/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import "TPAppDelegate.h"
+
+@implementation TPAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
